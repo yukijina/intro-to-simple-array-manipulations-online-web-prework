74d149eecdbecfcb46530a120a7d446cc35a990c
Yuki N <lunarbase@outlook.com>
2018-10-30 21:36:30 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,13 @@
-def push(array, string)
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
